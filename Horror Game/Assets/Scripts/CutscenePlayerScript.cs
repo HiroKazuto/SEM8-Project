ab33bec6a59562d5b14d1e01406ff4212e99f10e
Min Zay Ya <minzayya.augustinerichard@gmail.com>
2025-02-26 18:08:10 +0630

    Cutscene 1 is completed.
    You can tune object destroying timing.

--- Horror Game/Assets/Scripts/CutscenePlayerScript.cs
+++ Horror Game/Assets/Scripts/CutscenePlayerScript.cs
@@ -5,7 +5,12 @@ public class CutscenePlayerScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public PlayableDirector playableDirector;
-
+    public GameObject monsterObject;
+    [SerializeField] float delay = 5f;
+    void Start()
+    {
+        monsterObject.SetActive(false);
+    }
     void Update()
     {
         playableDirector = GetComponent<PlayableDirector>();
@@ -13,7 +18,9 @@ public class CutscenePlayerScript : MonoBehaviour
 
     public void PlayCutscene()
     {
+        monsterObject.SetActive(true);
         playableDirector.Play();
+        Destroy(monsterObject, delay);
     }
 
 }
